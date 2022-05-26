3577f2a989b8aef2e477ed298b4383f7b4961655
Caio <caiorochapstrlrd@gmail.com>
2022-05-26 02:31:15 -0300

    feat: add json as sysarg and possible output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 from extractors.vultr_extractor import VultrExtractor
 
@@ -9,3 +10,12 @@ if __name__ == '__main__':
     if '--print' in sys.argv:
         for machine in vultr_machines:
             print(machine.to_generic_machine())
+
+    if '--json' in sys.argv:
+        with open('machines.json', 'w', encoding='utf-8') as output_file:
+            json.dump(
+                [machine.to_generic_machine().__dict__ for machine in vultr_machines],
+                output_file
+            )
+
+    
