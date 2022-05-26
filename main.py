86e6d802ad468becddea6ece293935080371f31a
Caio <caiorochapstrlrd@gmail.com>
2022-05-26 02:15:34 -0300

    feat: add command line argument for printing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+
 from extractors.vultr_extractor import VultrExtractor
 
 if __name__ == '__main__':
     vultrExtractor = VultrExtractor('https://www.vultr.com/products/bare-metal/#pricing')
-    vultrExtractor.extract()
+    vultr_machines = vultrExtractor.extract()
+
+    if '--print' in sys.argv:
+        for machine in vultr_machines:
+            print(machine.to_generic_machine())
