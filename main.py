d24ddcda7ddd5b2a0a9aa0cf0f52258024b14272
Caio <caiorochapstrlrd@gmail.com>
2022-05-26 02:35:16 -0300

    refactor: extract generic machines to upper level on main

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ from extractors.vultr_extractor import VultrExtractor
 if __name__ == '__main__':
     vultrExtractor = VultrExtractor('https://www.vultr.com/products/bare-metal/#pricing')
     vultr_machines = vultrExtractor.extract()
+    generic_machines = [vultr_machine.to_generic_machine() for vultr_machine in vultr_machines]
 
     if '--print' in sys.argv:
-        for machine in vultr_machines:
-            print(machine.to_generic_machine())
+        for machine in generic_machines:
+            print(machine)
 
     if '--json' in sys.argv:
         with open('machines.json', 'w', encoding='utf-8') as output_file:
             json.dump(
-                [machine.to_generic_machine().__dict__ for machine in vultr_machines],
+                [machine.__dict__ for machine in generic_machines],
                 output_file
             )
-
-    
