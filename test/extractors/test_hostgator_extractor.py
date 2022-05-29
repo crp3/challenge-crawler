3c7ca842c7a7b01b50459897687f245c5ac717a6
Caio <caiorochapstrlrd@gmail.com>
2022-05-29 18:18:48 -0300

    test: add extractors test module and samples

---
+++ test/extractors/test_hostgator_extractor.py
@@ -0,0 +1,16 @@
+import unittest
+
+from models.hostgator_machine import HostgatorMachine
+from extractors.hostgator_extractor import HostgatorExtractor
+
+class TestHostgatorExtractor(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.extractor = HostgatorExtractor('')
+
+    def test_extract(cls):
+        with open('test/extractors/hostgator_page.html', 'r') as vultr_file:
+            cls.extractor.file = vultr_file
+            machines = cls.extractor.extract()
+            for machine in machines:
+                assert type(machine) == HostgatorMachine
---
+++ test/extractors/test_vultr_extractor.py
@@ -0,0 +1,16 @@
+import unittest
+
+from models.vultr_machine import VultrMachine
+from extractors.vultr_extractor import VultrExtractor
+
+class TestVultrExtractor(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.extractor = VultrExtractor('')
+    
+    def test_extract(cls):
+        with open('test/extractors/vultr_page.html', 'r') as vultr_file:
+            cls.extractor.file = vultr_file
+            machines = cls.extractor.extract()
+            for machine in machines:
+                assert type(machine) == VultrMachine
